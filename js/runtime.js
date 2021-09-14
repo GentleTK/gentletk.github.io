28fe12f717e6d3ac1863831e1558074c92330707
GentleTK <1144626145@qq.com>
2021-09-14 16:08:16 +0800

    Site updated: 2021-09-14 16:08:05

---
+++ js/runtime.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">function show_runtime(){window.setTimeout("show_runtime()",1000);X=new Date("08/15/2021 21:44:00");Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);runtime_span.innerHTML="在一起: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();</script>
